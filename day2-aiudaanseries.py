b3066edb249f58c75f258c01adbcd180a5ebf0f1
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2026-06-30 21:17:14 +0545

    first commit

---
+++ day2-aiudaanseries.py
@@ -0,0 +1,21 @@
+
+print("Bye world") #string
+print(123)  #number
+print(False)  #boolean
+
+bucket = 'water' #string
+cup = "coffee"  #string
+age = 24  #integer
+is_nepali = True #True/False boolean
+price = 24.12 #float
+
+magic_bucket = ["water","coffee","icecream"] #list
+
+fullName = "manish basnet"
+full_name = "manish basnet"
+FullName = "manish basnet"
+full-name = "manish basnet"
+
+
+#string --> testo kura jasma a to z or A to Z kei character aako xa vane
+
