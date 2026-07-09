ed80031c467cd7933fa995b56a8eff2c771827c0
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2026-07-09 09:08:09 +0545

    day 8

---
+++ python_learning/ai_udaan_day7.py
@@ -0,0 +1,16 @@
+
+
+numbers = [10,20,30,40,50]
+result = []
+
+for number in numbers:
+    result.append(number+5)
+
+print(result)
+#output --> [15,25,35,45,55]
+
+import numpy as np 
+numbers = np.array([10,20,30,40,50])
+print(numbers+5)
+
+#pandas --> data ko kura, read, analyze, organize, manipulate
